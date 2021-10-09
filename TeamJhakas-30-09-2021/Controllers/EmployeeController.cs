637382827e3e2c4382712901a204312b3de97764
yawar2019 <yawarali17@gmail.com>
2021-10-09 23:09:53 +0530

    Committed

--- TeamJhakas-30-09-2021/Controllers/EmployeeController.cs
+++ TeamJhakas-30-09-2021/Controllers/EmployeeController.cs
@@ -123,5 +123,94 @@ namespace TeamJhakas_30_09_2021.Controllers
 
             return RedirectToAction("index2", "New", listObj);
         }
+
+        public ActionResult sendDatausingModel()
+        {
+            string a = "asdfasd";
+            String b = "asdfa";
+
+            EmployeeModel obj = new EmployeeModel();
+            obj.EmpId = 1;
+            obj.EmpName = "Jack";
+            obj.EmpSalary = 99999;
+
+            return View(obj);
+        }
+
+        public ActionResult sendDatausingMultipleModel()
+        {
+            List<EmployeeModel> listObj = new List<EmployeeModel>();
+
+
+            EmployeeModel obj = new EmployeeModel();
+            obj.EmpId = 1;
+            obj.EmpName = "Jack";
+            obj.EmpSalary = 99999;
+
+            EmployeeModel obj1 = new EmployeeModel();
+
+            obj1.EmpId = 2;
+            obj1.EmpName = "Rupesh";
+            obj1.EmpSalary = 9999;
+
+
+            EmployeeModel obj2 = new EmployeeModel();
+            obj2.EmpId = 3;
+            obj2.EmpName = "Sushma";
+            obj2.EmpSalary = 39999;
+
+            listObj.Add(obj);
+            listObj.Add(obj1);
+            listObj.Add(obj2);
+
+            return View(listObj);
+        }
+
+        public ActionResult sendMixModel()
+        {
+            List<EmployeeModel> listObj = new List<EmployeeModel>();
+
+
+            EmployeeModel obj = new EmployeeModel();
+            obj.EmpId = 1;
+            obj.EmpName = "Jack";
+            obj.EmpSalary = 99999;
+
+            EmployeeModel obj1 = new EmployeeModel();
+
+            obj1.EmpId = 2;
+            obj1.EmpName = "Rupesh";
+            obj1.EmpSalary = 9999;
+
+
+            EmployeeModel obj2 = new EmployeeModel();
+            obj2.EmpId = 3;
+            obj2.EmpName = "Sushma";
+            obj2.EmpSalary = 39999;
+
+            listObj.Add(obj);
+            listObj.Add(obj1);
+            listObj.Add(obj2);
+
+
+            List<DepartmentModel> deptList = new List<DepartmentModel>();
+
+            DepartmentModel dept = new DepartmentModel();
+            dept.DeptId = 1;
+            dept.DeptName = "IT";
+
+            DepartmentModel dept1 = new DepartmentModel();
+            dept1.DeptId = 2;
+            dept1.DeptName = "Mechanical";
+
+            deptList.Add(dept);
+            deptList.Add(dept1);
+
+            EmpDept eobj = new EmpDept();
+            eobj.emp = listObj;
+            eobj.dept = deptList;
+
+            return View(eobj);
+        }
     }
 }
---
+++ TeamJhakas-30-09-2021/Models/EmpDept.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamJhakas_30_09_2021.Models
+{
+    public class EmpDept
+    {
+        public List<EmployeeModel> emp { get; set; }
+        public List<DepartmentModel> dept { get; set; }
+    }
+}
