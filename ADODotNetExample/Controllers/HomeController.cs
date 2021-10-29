50545244753d59466f7269e5ea7b961fba01eb57
yawar2019 <yawarali17@gmail.com>
2021-10-29 23:18:09 +0530

    committed

--- ADODotNetExample/Controllers/HomeController.cs
+++ ADODotNetExample/Controllers/HomeController.cs
@@ -32,5 +32,46 @@ namespace ADODotNetExample.Controllers
             }
             return View();
         }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            EmployeeModel emp = db.getEmployeesDataById(id);
+
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EmployeeModel emp)
+        {
+            int result = db.UpdateEmployee(emp);
+            if (result > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(emp);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            EmployeeModel emp = db.getEmployeesDataById(id);
+
+            return View(emp);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            EmployeeModel emp = db.getEmployeesDataById(id);
+            int result = db.DeleteEmployee(id);
+
+            if (result > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(emp);
+        }
     }
 }   
--- ADODotNetExample/Models/EmployeeContext.cs
+++ ADODotNetExample/Models/EmployeeContext.cs
@@ -13,7 +13,7 @@ namespace ADODotNetExample.Models
         public List<EmployeeModel> getEmployeesData()
         {
             List<EmployeeModel> listObj = new List<EmployeeModel>();
-            SqlCommand cmd = new SqlCommand("uspgetEmployeeDetails_10pm",con);
+            SqlCommand cmd = new SqlCommand("uspgetEmployeeDetails_10pm", con);
             cmd.CommandType = CommandType.StoredProcedure;
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
@@ -44,5 +44,61 @@ namespace ADODotNetExample.Models
             con.Close();
             return result;
         }
+
+
+
+        public EmployeeModel getEmployeesDataById(int? id)
+        {
+            EmployeeModel emp = new EmployeeModel();
+
+            SqlCommand cmd = new SqlCommand("spr_getEmployeeDetailsbyId", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@empid", id);
+
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            foreach (DataRow dr in dt.Rows)//foreach(string a in v)string []v
+            {
+                emp.EmpId = Convert.ToInt32(dr[0]);
+                emp.EmpName = Convert.ToString(dr[1]);
+                emp.EmpSalary = Convert.ToInt32(dr[2]);
+            }
+
+            return emp;
+        }
+
+        public int UpdateEmployee(EmployeeModel emp)
+        {
+            SqlCommand cmd = new SqlCommand("spr_updateEmployeeDetails", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+
+            cmd.Parameters.AddWithValue("@Empid", emp.EmpId);
+            cmd.Parameters.AddWithValue("@EmpName", emp.EmpName);
+            cmd.Parameters.AddWithValue("@EmpSalary", emp.EmpSalary);
+
+            int result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+
+        public int DeleteEmployee(int? empid)
+        {
+            SqlCommand cmd = new SqlCommand("usp_DeleteEmployeeById", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+
+            cmd.Parameters.AddWithValue("@EmpId", empid);
+
+            int result = cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            return result;
+        }
+
     }
 }
